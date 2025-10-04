9bfba1f7b982bee6363b4ea00c14a7bd899305cd
Sabrina <sabrinamaraldev@gmail.com>
2025-10-03 22:40:37 -0300

    add home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+export default function Home() {
+  return (
+    <main className="home-hero">
+      <div className="layer"></div>
+      <div className="content">
+        <h2>You got the travel plans, we got the travel vans.</h2>
+        <p>Add adventure to your life by joining the #vanlife movement.</p>
+        <p>Rent the perfect van to make your perfect road trip.</p>
+        <Link to="/vans">Find your van</Link>
+      </div>
+    </main>
+  );
+}
