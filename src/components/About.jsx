4bcbf276e6cf772c794c01a7403d3377d35d41f6
Sabrina <sabrinamaraldev@gmail.com>
2025-10-04 23:02:44 -0300

    add about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,28 @@
+import { Link } from "react-router-dom";
+
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <>
+      <img className="about-img" src="src/assets/about_picture.png"></img>
+      <div className="about-container">
+        <div className="about-content">
+          <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+          <p>
+            Our mission is to enliven your road trip with the perfect travel van
+            rental. Our vans are recertified before each trip to ensure your
+            travel plans can go off without a hitch. (Hitch costs extra 😉)
+          </p>
+          <p>
+            Our team is full of vanlife enthusiasts who know firsthand the magic
+            of touring the world on 4 wheels.
+          </p>
+        </div>
+        <div className="cfa-container">
+          <h2>Your destination is waiting.</h2>
+          <h2>Your van is ready.</h2>
+          <Link to="/vans">Explore our vans</Link>
+        </div>
+      </div>
+    </>
+  );
 }
