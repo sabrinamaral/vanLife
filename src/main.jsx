643fc0b63d2f716c2f4490f4db595a50031f8a48
Sabrina <sabrinamaraldev@gmail.com>
2025-10-06 00:06:38 -0300

    call the server function to start the server before the rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./components/App";
+import App from "./App";
+import { callServer } from "./server";
+
+callServer();
+console.log("call server!!");
 
 createRoot(document.getElementById("root")).render(<App />);
