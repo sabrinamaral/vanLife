fb1dc12b95978fb2c85458312e4c6be5ee93c9bf
Sabrina <sabrinamaraldev@gmail.com>
2025-10-11 23:12:48 -0300

    add subroutes using children's route and outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";
-import Nav from "./components/Nav";
 import About from "./pages/About";
+import Dashboard from "./pages/host/Dashboard";
 import Home from "./pages/Home";
-import Vans from "./pages/Vans";
-import VanDetail from "./pages/VanDetail";
+import HostLayout from "./components/HostLayout";
+import Income from "./pages/host/Income";
+import Layout from "./components/Layout";
+import Reviews from "./pages/host/Reviews";
+import Vans from "./pages/vans/Vans";
+import VanDetail from "./pages/vans/VanDetail";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="" element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
