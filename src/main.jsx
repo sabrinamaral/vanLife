41b160c2d2d1c65d90c9448e193021f0e8347cb2
Sabrina <sabrinamaraldev@gmail.com>
2025-10-12 13:34:52 -0300

    add useContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { callServer } from "./server";
+import { createContext } from "react";
+import { createRoot } from "react-dom/client";
+import { VansProvider } from "./context/VansProvider";
 
 callServer();
-console.log("call server!!");
+export const VansContext = createContext(null);
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <VansProvider>
+    <App />
+  </VansProvider>
+);
