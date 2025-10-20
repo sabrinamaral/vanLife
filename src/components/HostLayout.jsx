26fbd54102ddad05755d0a96b576eb60d9165948
Sabrina <sabrinamaraldev@gmail.com>
2025-10-19 23:35:16 -0300

    refactor navlink to add isActive feature

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,12 +1,34 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function HostLayout() {
   return (
     <>
       <nav className="host-layout">
-        <Link to="/host">Dashboard</Link>
-        <Link to="/host/income">Income</Link>
-        <Link to="/host/reviews">Reviews</Link>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          end
+          to="."
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="income"
+        >
+          Income
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="vans"
+        >
+          Vans
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="reviews"
+        >
+          Reviews
+        </NavLink>
       </nav>
       <Outlet />
     </>
