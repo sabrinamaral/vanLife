457aafd2780a0f6a3023e0ee63ba2ec2bb8cab61
Sabrina <sabrinamaraldev@gmail.com>
2025-11-12 19:39:58 -0300

    add .env to handle the password for the fake server to comply with GitGuardian

--- src/server.js
+++ src/server.js
@@ -77,7 +77,7 @@ createServer({
     server.create("user", {
       id: "123",
       email: "b@b.com",
-      password: "p123",
+      password: import.meta.env.VITE_PASSWORD,
       name: "Bob",
     });
   },
@@ -87,7 +87,7 @@ createServer({
     this.logging = false;
     // this.timing = 2000  // => mock a 2 second delay in server response
 
-    this.get("/vans", (schema, request) => {
+    this.get("/vans", (schema) => {
       // return new Response(400, {}, {error: "Error fetching data"})
       return schema.vans.all();
     });
@@ -97,7 +97,7 @@ createServer({
       return schema.vans.find(id);
     });
 
-    this.get("/host/vans", (schema, request) => {
+    this.get("/host/vans", (schema) => {
       // Hard-code the hostId for now
       return schema.vans.where({ hostId: "123" });
     });
