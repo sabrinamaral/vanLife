a53fd1e89182ef12304222d4acc2b64eb9c01a49
Sabrina <sabrinamaraldev@gmail.com>
2025-11-12 19:21:55 -0300

    add Login page and update routes to include login route and AuthRequired layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import HostVans from "./pages/host/HostVans";
 import HostVanDetail from "./pages/host/HostVanDetail";
 import Income from "./pages/host/Income";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
 import Reviews from "./pages/host/Reviews";
 import Vans from "./pages/vans/Vans";
 import VanDetail from "./pages/vans/VanDetail";
 import HostVansInfo from "./pages/host/HostVansInfo";
 import HostVansPricing from "./pages/host/HostVansPricing";
 import HostVansPhotos from "./pages/host/HostVansPhotos";
+import AuthRequired from "./components/AuthRequired";
 import HostLayout from "./components/HostLayout";
 import Layout from "./components/Layout";
 
@@ -24,17 +26,19 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route element={<AuthRequired />}>
+            <Route path="/host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
 
-          <Route path="/host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="vans" element={<HostVans />} />
-
-            <Route path="vans/:id" element={<HostVanDetail />}>
-              <Route index element={<HostVansInfo />} />
-              <Route path="pricing" element={<HostVansPricing />} />
-              <Route path="photos" element={<HostVansPhotos />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVansInfo />} />
+                <Route path="pricing" element={<HostVansPricing />} />
+                <Route path="photos" element={<HostVansPhotos />} />
+              </Route>
             </Route>
           </Route>
           <Route path="*" element={<NotFound />} />
